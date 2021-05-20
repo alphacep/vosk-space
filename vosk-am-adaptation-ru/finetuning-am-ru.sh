d366ee6931ed7f081424ab313dd5918717030eac
Sanzhar <48170101+SanzharMrz@users.noreply.github.com>
2021-05-20 18:40:19 +0600

    Update finetuning-am-ru.sh

--- vosk-am-adaptation-ru/finetuning-am-ru.sh
+++ vosk-am-adaptation-ru/finetuning-am-ru.sh
@@ -1,11 +1,13 @@
-. ./cmd.sh
-
 data_set=train
+test_set=test
 data_dir=data/${data_set}
+test_dir=data/${test_set}
 ali_dir=exp/${data_set}_ali
 src_dir=exp/nnet3/tdnn_sp
 dir=${src_dir}_${data_set}
 ivector_dir=exp/nnet3_online/ivectors_test
+
+. ./cmd.sh
 
 num_jobs_initial=1
 num_jobs_final=1
@@ -19,12 +21,51 @@ stage=1
 train_stage=-10
 nj=1
 
+echo -----
+echo 0. Get VOSK model.
+echo -----
+
+# Download model
 if [ $stage -le 1 ]; then
+  # get vosk
+  echo Downloading vosk-model-ru-0.10 ...
+  wget https://alphacephei.com/vosk/models/vosk-model-ru-0.10.zip
   
+  #unzip
+  echo Unzipping vosk-model-ru-0.10
+  unzip vosk-model-ru-0.10.zip 
+fi
+
+echo -----
+echo 1. Prepare data.
+echo -----
+
+# Download The M-AILABS Speech Dataset [3.6GB]
+if [ $stage -le 2 ]; then
+  # get dataset
+  echo Downloading M-AILABS dataset ...
+  wget http://www.caito.de/data/Training/stt_tts/ru_RU.tgz
+  
+  # untar
+  echo Untar M-AILABS dataset ...
+  tar -xvzf ru_RU.tgz
+  
+  # parse data in kaldi format
+  echo Creating Kaldi format data from ru_RU/by_book/male/minaev/oblomov/ ...
+  python create_data.py ru_RU/by_book/male/minaev/oblomov/ $data_dir $test_dir
+fi
+
+echo -----
+echo 2. Create features.
+echo -----
+
+if [ $stage -le 3 ]; then
+  # compute mfcc features
   steps/make_mfcc.sh \
     --cmd "$train_cmd" --nj $nj --mfcc-config conf/mfcc.conf \
     ${data_dir} exp/make_mfcc/${data_set} mfcc
-
+   
+  # normalize 
   steps/compute_cmvn_stats.sh ${data_dir} exp/make_mfcc/${data_set} mfcc
   utils/fix_data_dir.sh ${data_dir} || exit 1;
   
@@ -32,24 +73,27 @@ if [ $stage -le 1 ]; then
   sh steps/online/nnet2/extract_ivectors_online.sh $data_dir ivector ivector_dir
   
   # extract align features
-  sh steps/nnet3/align.sh $data_dir data/lang am $ali_dir  
+  sh steps/nnet3/align.sh $data_dir data/lang am $ali_dir
+  
+  # extract lats with generate_ali_from_lats=true
   sh steps/nnet3/align_lats.sh $data_dir data/lang am $ali_dir  
-
 fi
 
 echo -----
-echo 2
+echo 3. Copy model.
 echo -----
 
-if [ $stage -le 2 ]; then
+if [ $stage -le 4 ]; then
+  # copy model in raw format
   utils/run.pl $dir/log/generate_input_model.log nnet3-am-copy --raw=true "am/final.mdl" "$dir/input.raw";
 fi
 
 echo -----
-echo 3
+echo 4. Train model.
 echo -----
 
-if [ $stage -le 3 ]; then
+if [ $stage -le 5 ]; then
+  # train model
   steps/nnet3/train_dnn.py --stage=$train_stage \
     --cmd="$decode_cmd" \
     --feat.cmvn-opts="--norm-means=false --norm-vars=false" \
