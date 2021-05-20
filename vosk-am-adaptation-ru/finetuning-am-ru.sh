77e517faf4b1e55e47b2a53c70cc0f6edd755e5b
Sanzhar <48170101+SanzharMrz@users.noreply.github.com>
2021-05-20 14:44:55 +0600

    Update finetuning-am-ru.sh

--- vosk-am-adaptation-ru/finetuning-am-ru.sh
+++ vosk-am-adaptation-ru/finetuning-am-ru.sh
@@ -5,32 +5,35 @@ data_dir=data/${data_set}
 ali_dir=exp/${data_set}_ali
 src_dir=exp/nnet3/tdnn_sp
 dir=${src_dir}_${data_set}
+ivector_dir=exp/nnet3_online/ivectors_test
 
 num_jobs_initial=1
 num_jobs_final=1
 num_epochs=1
-initial_effective_lrate=0.000005
-final_effective_lrate=0.000001
-minibatch_size=128
+initial_effective_lrate=0.000025
+final_effective_lrate=0.00001
+minibatch_size=512
 
 stage=1
+
 train_stage=-10
 nj=1
 
 if [ $stage -le 1 ]; then
+  
   steps/make_mfcc.sh \
-    --cmd "$train_cmd" --nj $nj \
+    --cmd "$train_cmd" --nj $nj --mfcc-config conf/mfcc.conf \
     ${data_dir} exp/make_mfcc/${data_set} mfcc
-  steps/compute_cmvn_stats.sh ${data_dir} exp/make_mfcc/${data_set} mfcc || exit 1;
 
+  steps/compute_cmvn_stats.sh ${data_dir} exp/make_mfcc/${data_set} mfcc
   utils/fix_data_dir.sh ${data_dir} || exit 1;
-
+  
   # extract ivector features
-  sh steps/online/nnet2/extract_ivectors_online.sh $data_dir ivector exp/nnet3_online/ivectors_test
+  sh steps/online/nnet2/extract_ivectors_online.sh $data_dir ivector ivector_dir
   
   # extract align features
-  sh steps/nnet3/align.sh $data_dir data/lang am $ali_dir
-  sh steps/nnet3/align_lats.sh $data_dir data/lang am $ali_dir
+  sh steps/nnet3/align.sh $data_dir data/lang am $ali_dir  
+  sh steps/nnet3/align_lats.sh $data_dir data/lang am $ali_dir  
 
 fi
 
@@ -39,7 +42,7 @@ echo 2
 echo -----
 
 if [ $stage -le 2 ]; then
-  $train_cmd $dir/log/generate_input_model.log nnet3-am-copy --raw=true "$am/final.mdl" "$dir/input.raw";
+  utils/run.pl $dir/log/generate_input_model.log nnet3-am-copy --raw=true "am/final.mdl" "$dir/input.raw";
 fi
 
 echo -----
@@ -57,10 +60,10 @@ if [ $stage -le 3 ]; then
     --trainer.optimization.initial-effective-lrate $initial_effective_lrate \
     --trainer.optimization.final-effective-lrate $final_effective_lrate \
     --trainer.optimization.minibatch-size $minibatch_size \
-    --feat-dir ${data_dir} \
+    --feat-dir ${data_dir}\
     --lang data/lang \
     --ali-dir ${ali_dir} \
-    --feat.online-ivector-dir exp/nnet3_online/ivectors_test \
+    --feat.online-ivector-dir ivector_dir
     --egs.frames-per-eg 8 \
     --dir $dir || exit 1;
 fi
